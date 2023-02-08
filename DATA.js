81be5a4086ad78450fe2f26e6bea7d22fe477e7a
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-08 12:59:50 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,35 @@
+export const item = {
+    "id": "6586a8f8-4f65-4fd8-ac1b-1b06a14be409",
+    "product_list": [
+        {
+            "id": "ff001dd9-e074-4822-918c-1d7f96f6db8a",
+            "price": "also",
+            "product_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png",
+            "about_product": "Trouble area society nature sing store. Hand drive start director trouble."
+        },
+        {
+            "id": "e1f59e96-3b97-43c9-88a8-fdaf9c70b0e8",
+            "price": "interest",
+            "product_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png",
+            "about_product": "Month us conference recent thank. Class time be necessary three step newspaper."
+        },
+        {
+            "id": "e8d2c524-ac85-4985-9c6a-50b906d2d299",
+            "price": "training",
+            "product_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png",
+            "about_product": "Seem table yard trip. Red race place series than whatever central. Candidate including owner treatment claim today white."
+        },
+        {
+            "id": "c163b273-2d54-4857-8c12-4fe3a8922da5",
+            "price": "mind",
+            "product_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/242.png",
+            "about_product": "Sister south dream attack job instead. Toward operation support table see computer picture. Small place teach agreement."
+        },
+        {
+            "id": "793e8c94-16fa-4015-bfb3-8ab0c9dc6844",
+            "price": "energy",
+            "product_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png",
+            "about_product": "Enough summer society popular indicate. Produce read security carry."
+        }
+    ]
+}
